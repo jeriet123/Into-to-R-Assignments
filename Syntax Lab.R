c7f6a66c064fbcdac4b591550d783c77e5507c6c
“jeriet123” <jerietr@gmail.com>
2019-09-10 13:46:43 -0500

    Special Values

--- Syntax Lab.R
+++ Syntax Lab.R
@@ -19,3 +19,19 @@ x * y + z
 x * (y + z)
 # The outputs differ becaues r follows the order of operations so when y and z are in parenthesis they are added first other wise
 # y is multiplied by x first
+infinity <- 3/0
+infinity
+# inf is returned when a positive value is divided by zero because the computer can't divide by zero
+ninfinity <- -3/0
+ninfinity
+# -inf is the same as inf but the number is negative
+Not_A_Number <- sqrt(-3)
+Not_A_Number
+# NAN is returned when the square root of -3 is given because the returned value it not a real number and the computer knows this
+Not_Applicable <- matrix()
+Not_Applicable
+# NA is returned because a value necessary for a function to work is missing or not available for the finction to use
+Nulification <- function(x){}
+N <- F(3)
+N
+# NULL is returned when a Function contained undefined values. like when the function is empty
