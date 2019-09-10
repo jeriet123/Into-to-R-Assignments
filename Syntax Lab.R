b2642662d53a446c9d3e865d244e56f36c50bcba
“jeriet123” <jerietr@gmail.com>
2019-09-10 12:50:09 -0500

    %% and the %/% Operators

--- Syntax Lab.R
+++ Syntax Lab.R
@@ -4,3 +4,7 @@ Char <- "Boy"
 Int <- 4
 Num <- 4.5
 Fac <- factor(c("Boy", "Girl"))
+# The %% operator divides the value and returns the remainder
+Remainder <- Num %% Int
+# the %/% operator divides the values and returns the dividend
+Dividend <- Num %/% Int
